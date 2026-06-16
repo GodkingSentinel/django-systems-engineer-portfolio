271e31fd29609558c7baaa9a49a7b68491f1f6be
Richard Viens <rviens2@wgu.edu>
2026-06-15 23:49:49 -1000

    Fix homepage URL routing

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import path, include
+from profilepage import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('profilepage.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("", include("django.contrib.auth.urls")),
+    path("staff/", views.staff_dashboard, name="staff_dashboard"),
 ]
