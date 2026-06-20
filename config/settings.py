8fe5698cf20ee401485e8fb57095b51f94125fed
Richard Viens <rviens2@wgu.edu>
2026-06-20 11:50:01 -1000

    Fix login redirect after recruiter login

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/#projects"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
