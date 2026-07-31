015b2cad66e81e72a843e8fccb0fc8bc417bc41f
GodkingSentinel <125913510+GodkingSentinel@users.noreply.github.com>
2026-07-31 10:36:56 -1000

    Add URL path for downloading recommendation letter

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", views.home, name="home"),
     path("staff-dashboard/", views.staff_dashboard, name="staff_dashboard"),
+    path(
+        "download/recommendation-letter/",
+        views.download_recommendation_letter,
+        name="download_recommendation_letter",
+    ),
 ]
