f32cf2cf90ca0b4c94a7658b4fc078e205f55644
GodkingSentinel <125913510+GodkingSentinel@users.noreply.github.com>
2026-07-31 10:20:27 -1000

    Remove duplicate import of login_required

--- profilepage/views.py
+++ profilepage/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from pathlib import Path
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
