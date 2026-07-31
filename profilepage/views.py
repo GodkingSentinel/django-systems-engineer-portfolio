13107adcb6b6f819e5984ea2cbdeb1f79ecb7fa1
GodkingSentinel <125913510+GodkingSentinel@users.noreply.github.com>
2026-07-31 10:19:44 -1000

    Update views.py

--- profilepage/views.py
+++ profilepage/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from pathlib import Path
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import FileResponse, Http404
 
 
 def home(request):
@@ -10,3 +14,21 @@ def home(request):
 def staff_dashboard(request):
     return render(request, "profilepage/staff_dashboard.html")
 
+
+@login_required
+def download_recommendation_letter(request):
+    file_path = (
+        Path(settings.BASE_DIR)
+        / "profilepage"
+        / "private_documents"
+        / "Richard_Viens_Recommendation_Letter.pdf"
+    )
+
+    if not file_path.exists():
+        raise Http404("Recommendation letter not found.")
+
+    return FileResponse(
+        open(file_path, "rb"),
+        as_attachment=True,
+        filename="Richard_Viens_Recommendation_Letter.pdf",
+    )
