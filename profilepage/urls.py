f95ff8f86c11be3e9346182f11d7ec89a330e4f4
GodkingSentinel <125913510+GodkingSentinel@users.noreply.github.com>
2026-07-31 10:26:51 -1000

    Update URL patterns for staff dashboard and add download

--- profilepage/urls.py
+++ profilepage/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('staff/', views.staff_dashboard, name='staff_dashboard'),
+    path("", views.home, name="home"),
+    path("staff-dashboard/", views.staff_dashboard, name="staff_dashboard"),
+    path(
+        "download/recommendation-letter/",
+        views.download_recommendation_letter,
+        name="download_recommendation_letter",
+    ),
 ]
